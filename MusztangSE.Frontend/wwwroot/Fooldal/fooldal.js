f29bbcfcb01531b1421cf8807f43ed6d5a588315
Daniel <zsitva.daniel.bela@ganziskola.hu>
2026-02-04 15:35:51 +0100

    Fő cim karakterenkénti kiiratása 2026.02.04

---
+++ MusztangSE.Frontend/wwwroot/Fooldal/fooldal.js
@@ -0,0 +1,31 @@
+﻿// Ki irja a MusztángSE szöveget karakterenként
+const szoveg = "MusztángSE";
+const cim = document.getElementById("cim");
+
+let index = 0;
+
+function kiir() {
+    // Ha kiirta akkor leáll
+    if (index >= szoveg.length) return;
+
+    const karakter = szoveg[index];
+
+    
+    if (karakter === " ") {
+        cim.innerHTML += " ";
+        index++;
+        kiir();
+        return;
+    }
+
+    // Szín eldöntése Musztáng rész fehér az SE rész piros a 8 karaktertöl kezdve piros addig fehér
+    const osztaly = index < 8 ? "feher" : "piros";
+
+    cim.innerHTML += `<span class="${osztaly}">${karakter}</span>`;
+
+    index++;
+    setTimeout(kiir, 180); // kicsit gyorsabb, természetesebb
+}
+
+// Oldal betöltése után indul
+window.addEventListener("load", kiir);
